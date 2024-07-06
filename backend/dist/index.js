2840e0d0671284c26a0331368e729491bc010d70
vishal-gohil12 <21it039@charusat.edu.in>
2024-07-06 21:25:00 +0530

    change

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -6,24 +6,61 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = require("dotenv");
 const cors_1 = __importDefault(require("cors"));
-const user_1 = require("./routes/user");
+const socket_io_1 = require("socket.io");
+const room_1 = require("./routes/room");
 (0, dotenv_1.config)();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 8080;
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use("/api/v1/room", room_1.roomRoute);
 const httpServer = app.listen(PORT, () => {
     console.log("Server is running on port", PORT);
 });
-app.use((0, cors_1.default)());
-app.use(express_1.default.json());
-app.use("/api/v1/user", user_1.userRoute);
-app.get("/auth", (req, res) => {
-    res.status(200).json({ message: "You have access to this protected route!" });
+const io = new socket_io_1.Server(httpServer);
+const socketMap = {};
+const getAllClients = (roomId) => {
+    return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map((socketId) => {
+        return {
+            socketId,
+            username: socketMap[socketId]
+        };
+    });
+};
+io.on('connection', (socket) => {
+    console.log(socket.id);
+    socket.on("join", ({ roomId, username }) => {
+        socketMap[socket.id] = username;
+        socket.join(roomId);
+        const client = getAllClients(roomId);
+        client.forEach(({ socketId }) => {
+            io.to(socketId).emit("joined", {
+                socketId: socket.id,
+                username,
+                client
+            });
+        });
+    });
+    socket.on("disconnecting", () => {
+        const rooms = [...socket.rooms];
+        rooms.forEach((roomId) => {
+            socket.in(roomId).emit('leave', {
+                socket: socket.id,
+                username: socketMap[socket.id]
+            });
+            socket.leave(socket.id);
+        });
+        delete socketMap[socket.id];
+    });
+    socket.on('code-change', ({ roomId, code }) => {
+        socket.in(roomId).emit('code-change', { code });
+    });
+    socket.on('code-sync', ({ code, socketId }) => {
+        socket.in(socketId).emit('change', {
+            code
+        });
+    });
+    socket.on('output-change', ({ roomId, output }) => {
+        socket.in(roomId).emit('output-change', { output });
+    });
 });
-// const wss = new WebSocketServer({ server: httpServer});
-// wss.on('connection', (ws)=>{
-//     ws.on('error', console.error);
-//     ws.on('message', function message(data, isBinary) {
-//         console.log(data.toString());
-//     });
-//     ws.send("Hello from webSocket")
-// })
--- backend/dist/routes/room.js
+++ backend/dist/routes/room.js
@@ -1,1 +1,26 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.roomRoute = void 0;
+const express_1 = require("express");
+const child_process_1 = require("child_process");
+exports.roomRoute = (0, express_1.Router)();
+exports.roomRoute.post('/execute', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { code } = req.body;
+    const safeCode = code.replace(/"/g, '\\"').replace(/\n/g, '\\n');
+    (0, child_process_1.exec)(`node -e "${safeCode}`, (error, stdout, stderr) => {
+        if (error) {
+            return res.status(500).json({ output: stderr });
+        }
+        console.log(stdout);
+        res.json({ output: stdout });
+    });
+}));
